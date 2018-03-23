12ef63fe888acad13d0a6200efea349c400d588e
gnaponie <gnaponie@redhat.com>
2018-03-23 15:58:09 +0100

    Launching in foreground the bareos fd

--- conf_script.sh
+++ conf_script.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -x
 
 # changing conf taking the values from the env
 TMPDIR=. sed -i -e "s/DIRECTOR_PASSWORD/$DIRECTOR_PASSWORD/g" /etc/bareos/bareos-fd.conf
@@ -7,5 +6,4 @@ TMPDIR=. sed -i -e "s/DIRECTOR_NAME/$DIRECTOR_NAME/g" /etc/bareos/bareos-fd.conf
 TMPDIR=. sed -i -e "s/FD_NAME/$FD_NAME/g" /etc/bareos/bareos-fd.conf
 
 # start bareos-fd
-bareos-fd
-sleep infinity
+bareos-fd -f
