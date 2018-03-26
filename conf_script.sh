db6e636775e0c5e054fef7d8ac1ed1fa0411290f
gnaponie <gnaponie@redhat.com>
2018-03-26 13:40:12 +0200

    Change permissions to /var/lib/bareos dir

--- conf_script.sh
+++ conf_script.sh
@@ -6,6 +6,5 @@ TMPDIR=. sed -i -e "s/DIRECTOR_NAME/$DIRECTOR_NAME/g" /etc/bareos/bareos-fd.conf
 TMPDIR=. sed -i -e "s/FD_NAME/$FD_NAME/g" /etc/bareos/bareos-fd.conf
 
 # start bareos-fd
-touch /var/lib/bareos/bareos-fd.9102.pid
-chmod 777 /var/lib/bareos/bareos-fd.9102.pid
+chmod -R ugo+rw /var/lib/bareos
 bareos-fd -f
