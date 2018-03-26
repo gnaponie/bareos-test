b91bab1b79275f99d30bb560702483df3fa7d7d8
gnaponie <gnaponie@redhat.com>
2018-03-26 15:05:27 +0200

    Added some more debug info

--- conf_script.sh
+++ conf_script.sh
@@ -6,4 +6,4 @@ TMPDIR=. sed -i -e "s/DIRECTOR_NAME/$DIRECTOR_NAME/g" /etc/bareos/bareos-fd.conf
 TMPDIR=. sed -i -e "s/FD_NAME/$FD_NAME/g" /etc/bareos/bareos-fd.conf
 
 # start bareos-fd
-bareos-fd -f -v
+bareos-fd -f -v -d 10
