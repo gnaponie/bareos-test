98fb8be1faedb054e72f87d4702240fcd7708219
gnaponie <gnaponie@redhat.com>
2018-04-10 14:28:45 +0200

    Increased debugging prints

--- conf_script.sh
+++ conf_script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -v
+
 # changing conf taking the values from the env
 TMPDIR=. sed -i -e "s/DIRECTOR_PASSWORD/$DIRECTOR_PASSWORD/g" /etc/bareos/bareos-fd.conf
 TMPDIR=. sed -i -e "s/DIRECTOR_NAME/$DIRECTOR_NAME/g" /etc/bareos/bareos-fd.conf
