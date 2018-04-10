c150dc5e862b2b32873e13a1e895862520057ac2
gnaponie <gnaponie@redhat.com>
2018-04-10 14:41:37 +0200

    Changing sed delimiter

--- conf_script.sh
+++ conf_script.sh
@@ -3,7 +3,8 @@
 set -v
 
 # changing conf taking the values from the env
-TMPDIR=. sed -i -e "s/DIRECTOR_PASSWORD/$DIRECTOR_PASSWORD/g" /etc/bareos/bareos-fd.conf
+# using the "|" here instead of the "/" because there might be a "/" in the password
+TMPDIR=. sed -i -e "s|DIRECTOR_PASSWORD|$DIRECTOR_PASSWORD|g" /etc/bareos/bareos-fd.conf
 TMPDIR=. sed -i -e "s/DIRECTOR_NAME/$DIRECTOR_NAME/g" /etc/bareos/bareos-fd.conf
 TMPDIR=. sed -i -e "s/FD_NAME/$FD_NAME/g" /etc/bareos/bareos-fd.conf
 
